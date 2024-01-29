20de47245a4d7e0b57283e99c8e97fb3831645e4
MonoSetton <maksymiliankloch@gmail.com>
2024-01-29 14:31:09 +0100

    Update settings for railway

--- album_app/settings.py
+++ album_app/settings.py
@@ -151,5 +151,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / 'emails'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ${{shared.Email}}
+EMAIL_HOST_PASSWORD = ${{shared.Password}}
+
+
